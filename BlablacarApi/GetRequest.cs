bfd59b59281bad69f24ee2baec44d74ef02c3469
Ярослав Меньшиков <mjs2001@bk.ru>
2021-07-31 01:42:05 +0700

    Добавьте файлы проекта.

---
+++ BlablacarApi/GetRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace BlablacarApi
+{
+    public class GetRequest
+    {
+        HttpWebRequest _request;
+        string _address;
+
+        public string Response { get; set; }
+
+        public GetRequest(string address)
+        {
+            _address = address;
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "Get";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception)
+            {
+            }
+
+
+        }
+    }
+}
---
+++ BlablacarApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace BlablacarApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "bzxKThZ8TWYxCyX6r5Juzh6omErxATBC";
+            var data = DateTime.Now.ToString("yyyy-MM-ddT00:00:00");
+            var coor1 = "48.864716,2.349014";
+            var coor2 = "45.75801,4.8000158";
+            var currency = "RUB";
+
+            var request = new GetRequest($"https://public-api.blablacar.com/api/v3/trips?from_coordinate={coor1}&to_coordinate={coor2}&locale=fr-FR&currency={currency}&start_date_local={data}&key={key}");
+            request.Run();
+
+            var response = request.Response;
+
+            var json = JObject.Parse(response);
+            var trips = json["trips"];
+
+            foreach (var trip in trips)
+            {
+                var price = trip["price"];
+                var amount = price["amount"];
+
+                Console.WriteLine($"price={amount} руб.");
+            }
+
+        }
+    }
+}
