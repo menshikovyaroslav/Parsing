7e7bd95c44a53906cffcecb4ea3526def9060cc6
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-09-06 03:13:17 +0700

    карточка товара + парсинг

---
+++ Baucenter.ru/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baucenter.ru
+{
+    public class Card
+    {
+        public string Price { get; set; }
+        public string Title { get; set; }
+
+        public void Parse(string html)
+        {
+            var priceStart = html.IndexOf("Цена") + 11;
+            var priceEnd = html.IndexOf("<span", priceStart);
+            Price = html.Substring(priceStart, priceEnd - priceStart).Trim();
+
+            var titleStart = html.IndexOf("<h1>") + 4;
+            var titleEnd = html.IndexOf("</h1>", titleStart);
+            Title = html.Substring(titleStart, titleEnd - titleStart).Trim();
+        }
+    }
+}
--- Baucenter.ru/Program.cs
+++ Baucenter.ru/Program.cs
@@ -12,7 +12,7 @@ namespace Baucenter.ru
     {
         static void Main(string[] args)
         {
-            var code = "416001653";
+            var code = "536000644";
             var proxy = new WebProxy("127.0.0.1:8888");
             var cookieContainer = new CookieContainer();
 
@@ -43,8 +43,6 @@ namespace Baucenter.ru
 
             Console.WriteLine($"getPath={getPath}");
 
-            Console.ReadKey();
-
             var getRequest = new GetRequest($"https://baucenter.ru{getPath}");
             getRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
             getRequest.Useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36";
@@ -58,6 +56,14 @@ namespace Baucenter.ru
             getRequest.Host = "baucenter.ru";
             getRequest.Proxy = proxy;
             getRequest.Run(cookieContainer);
+
+            var card = new Card();
+            card.Parse(getRequest.Response);
+
+            Console.WriteLine($"title={card.Title}");
+            Console.WriteLine($"price={card.Price}");
+
+            Console.ReadKey();
         }
     }
 }
